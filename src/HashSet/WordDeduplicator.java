c01a8ce465b2697940db3b83671bb218435d8a5d
ahin0422 <ahinahn0422@gmail.com>
2026-07-25 15:31:18 +0900

    add

--- src/HashSet/WordDeduplicator.java
+++ src/HashSet/WordDeduplicator.java
@@ -1,4 +1,24 @@
 package HashSet;
 
+import java.util.HashSet;
+
 public class WordDeduplicator {
+    HashSet<String> set = new HashSet<>();
+
+    void add(String name) {
+        set.add(name);
+    }
+
+    void see() {
+        System.out.println(set);
+    }
+
+    void remove(String name) {
+        if (set.contains(name)) {
+            set.remove(name);
+        }
+        else {
+            System.out.println("I Can't Find");
+        }
+    }
 }
--- src/HashSet/WordDeduplicatorMain.java
+++ src/HashSet/WordDeduplicatorMain.java
@@ -1,4 +1,51 @@
 package HashSet;
 
+import java.util.Scanner;
+
 public class WordDeduplicatorMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        WordDeduplicator word = new WordDeduplicator();
+
+        System.out.print("Number Of Name: ");
+        int a = scanner.nextInt();
+        for (int i = 0; i < a; i++) {
+            System.out.print("Add Name : ");
+            String name = scanner.next();
+            word.add(name);
+        }
+
+        System.out.print("Want To Remove? (yes or no) : ");
+        String say = scanner.next();
+        if (say == "yes") {
+            System.out.print("How Much? (Number) : ");
+            a = scanner.nextInt();
+            for (int i = 0; i < a; i++) {
+                System.out.print("Add Name : ");
+                String name = scanner.next();
+                word.remove(name);
+            }
+        }
+        else if (say == "no") {
+            System.out.println("Ok");
+        }
+        else {
+            System.out.println("I can't Understand");
+        }
+
+        System.out.println("Want To See? (yes or no) : ");
+        say = scanner.next();
+        if (say == "yes") {
+            System.out.println("Ok");
+            word.see();
+        }
+        else if (say == "no") {
+            System.out.println("It's Ok");
+        }
+        else {
+            System.out.println("I can't Understand");
+        }
+
+        System.out.println("END");
+    }
 }
