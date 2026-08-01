9c63aee6ec6b384f26cb8afc8d198d1f07fa097d
ahin0422 <ahinahn0422@gmail.com>
2026-08-01 23:50:14 +0900

    add

--- src/LinkedList/MyQueue.java
+++ src/LinkedList/MyQueue.java
@@ -1,4 +1,19 @@
 package LinkedList;
 
+import java.util.LinkedList;
+
 public class MyQueue {
+    LinkedList<Integer> list = new LinkedList<>();
+
+    void enqueue(int sum) {
+        list.add(sum);
+    }
+
+    void dequeue() {
+        System.out.println(list.removeFirst());
+    }
+
+    void isEmpty() {
+        System.out.println(list.isEmpty());
+    }
 }
--- src/LinkedList/MyQueueMain.java
+++ src/LinkedList/MyQueueMain.java
@@ -1,4 +1,14 @@
 package LinkedList;
 
+import java.util.Scanner;
+
 public class MyQueueMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        MyQueue myqueue = new MyQueue();
+        int a = scanner.nextInt();
+        myqueue.enqueue(a);
+        myqueue.dequeue();
+        myqueue.isEmpty();
+    }
 }
