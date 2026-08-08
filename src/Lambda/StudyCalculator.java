10f8f2f3f2182dc50e1e094c479e06345165cc34
ahin0422 <ahinahn0422@gmail.com>
2026-08-08 23:24:11 +0900

    add

--- src/Lambda/StudyCalculator.java
+++ src/Lambda/StudyCalculator.java
@@ -1,4 +1,12 @@
 package Lambda;
 
 public class StudyCalculator {
+    interface  Cacu {
+        int caculate(int a, int b);
+    }
+
+    Cacu add;
+    Cacu sub;
+    Cacu mul;
+    Cacu div;
 }
--- src/Lambda/StudyCalculatorMain.java
+++ src/Lambda/StudyCalculatorMain.java
@@ -1,5 +1,17 @@
 package Lambda;
 
 public class StudyCalculatorMain {
+    public static void main(String[] args) {
+        StudyCalculator studyCalculator = new StudyCalculator();
 
+        studyCalculator.add = (a,b) -> a + b;
+        studyCalculator.sub = (a,b) -> a - b;
+        studyCalculator.mul = (a,b) -> a * b;
+        studyCalculator.div = (a,b) -> a / b;
+
+        System.out.println(studyCalculator.add.caculate(2,5));
+        System.out.println(studyCalculator.sub.caculate(5,2));
+        System.out.println(studyCalculator.mul.caculate(2,5));
+        System.out.println(studyCalculator.div.caculate(10,2));
+    }
 }
